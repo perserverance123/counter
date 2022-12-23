ae53e1d0a486310e1b33f3a065d1177637239889
perserverance123 <121180358+perserverance123@users.noreply.github.com>
2022-12-23 14:37:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 let v=0;
+let player_count=0;
+let v_largest=0;
+let c_largest=0;
 let alpha=document.getElementById("count");
 let beta =  document.getElementById("prev_rec");
 function increment()
@@ -8,10 +11,23 @@ alpha.innerText=v;
 console.log("button was clicked");
 }
 function save()
-{
+{   player_count+=1;
     console.log(v);
-   
+    if(v_largest<v)
+{
+    v_largest=v;
+    c_largest=player_count;
+}
     alpha.innerText=v;
-    beta.innerText=beta.innerText+v+"-";
+    beta.innerText+="Player " +player_count +" :- "+v;
+    beta.innerHTML+="<br>";
+    
     v=0;
+}
+
+let topg=document.getElementById("end_match");
+function end_the_match()
+{
+    alert("congratulation player " +c_largest +" "+" you won" );
+    document.location.reload();
 }
